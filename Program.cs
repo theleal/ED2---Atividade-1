e1d572733e692cb6c634cfef84619ceedc55fcab
theleal <89053362+theleal@users.noreply.github.com>
2022-08-23 19:16:33 -0300

    Update Program.cs
    entendeu??w

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,74 +11,70 @@ namespace ED2.ATV1
         static void Main(string[] args)
         {
 
-            
-            Vendedores equipe = new Vendedores(2);
 
-            Vendedor palinkas = new Vendedor(1, "palinkas", 0.2);
-            Vendedor carol = new Vendedor(2, "carol", 0.3);
-            Vendedor leal = new Vendedor(3, "leal", 0.5);
 
-            Venda venda1 = new Venda(3, 300);
-            Venda venda2 = new Venda(2, 100);
+            Vendedores equipe = new Vendedores(10);
+
+            Vendedor palinkas = new Vendedor(1, "Palinkas", 0.2);
+            Venda venda1 = new Venda(7, 700);
 
             palinkas.registrarVenda(1, venda1);
-            carol.registrarVenda(1, venda2);
 
-            Console.WriteLine(equipe.addVendedor(palinkas) ? "otimo(Y)" : "deuruim");
-            Console.WriteLine(equipe.addVendedor(carol) ? "otimo(Y)" : "deuruim");
-            Console.WriteLine(equipe.addVendedor(leal) ? "otimo(Y)" : "deuruim");
+            equipe.addVendedor(palinkas);
 
-            Vendedor corna = equipe.searchVendedor(new Vendedor(2, "", 0));
-            Console.WriteLine("consulta:" + corna.ToString());
 
-            Console.WriteLine("o valor eh: " + equipe.valorVendas());
-
-            Console.WriteLine("o valor da comissao eh: " + equipe.valorComissao());
-
-            
-            Console.WriteLine(equipe.addVendedor(leal) ? "otimo(Y)" : "deuruim");
-
-            Console.WriteLine("o valor eh: " + equipe.valorVendas());
-
-            Console.WriteLine("o valor da comissao eh: " + equipe.valorComissao());
-
-            Console.WriteLine("\n\n========================================================================\n\n");
-
-            foreach (Vendedor vendedor in equipe.OsVendedores)
+            int resposta = -1;
+            while (resposta != 0)
             {
-                Console.WriteLine(vendedor.ToString() + "\n\n");
-            }
-
-
-            
-            {
-
-
-
-                int resposta = 0;
-                while (true) 
-                
-                { 
 
                 Console.WriteLine("\n0 - Finalizar programa\n1- Cadastrar vendedor\n2- Consultar vendedor\n3- Excluir vendedor\n4- Registrar venda\n5- Listar vendedores");
                 Console.WriteLine("Digite o número correspondente a opção desejada: ");
                 resposta = int.Parse(Console.ReadLine());
-                
-                    switch (resposta)
-                    {
-                        case 0:
-                            Environment.Exit(0);
-                            break;
-                        case 1:
-                            Vendedores vendedor = new Vendedores(10);
-                            Console.WriteLine(vendedor.searchVendedor(palinkas));
-                            
-                    }
-                
-                }
-            }
 
-            Console.ReadKey();
+                switch (resposta)
+                {
+                    case 0:
+                        Console.WriteLine("Sair");
+                        break;
+                    case 1:
+                        Console.WriteLine("Cadastrar");
+                        break;
+                    case 2:
+                        int Id;
+                        Console.WriteLine("Consultar");
+                        Console.WriteLine("Digite o ID do vendedor: ");
+                        
+                        Id = int.Parse(Console.ReadLine());
+                        Vendedor vendedor = new Vendedor(Id, "", 0);
+                        
+                        vendedor = equipe.searchVendedor(vendedor);
+                        
+                        if(vendedor.Id == 0)
+                        {
+                            Console.WriteLine("Vendedor não encontrado");
+                        }
+                        else
+                        {
+                            Console.WriteLine(vendedor.ToString());
+                        }
+
+                        break;
+                    case 3:
+                        Console.WriteLine("Excluir vendedor");
+                        break;
+                    case 4:
+                        Console.WriteLine("Registrar venda");
+                        break;
+                    case 5:
+                        Console.WriteLine("Listar vendedores");
+                        break;
+                    default:
+                        Console.WriteLine("opção invalida");
+                        break;
+
+                }
+
+            }
 
             //
         }
