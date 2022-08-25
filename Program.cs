a069588a973d5cddb3368533e0770ad6bc75d386
theleal <89053362+theleal@users.noreply.github.com>
2022-08-24 21:01:18 -0300

    Update - ATV1

--- Program.cs
+++ Program.cs
@@ -15,12 +15,12 @@ namespace ED2.ATV1
 
             Vendedores equipe = new Vendedores(10);
 
-            Vendedor palinkas = new Vendedor(1, "Palinkas", 0.2);
-            Venda venda1 = new Venda(7, 700);
+            //Vendedor palinkas = new Vendedor(1, "Palinkas", 0.2);
+            //Venda venda1 = new Venda(7, 700);
 
-            palinkas.registrarVenda(1, venda1);
+            //palinkas.registrarVenda(1, venda1);
 
-            equipe.addVendedor(palinkas);
+            //equipe.addVendedor(palinkas);
 
 
             int resposta = -1;
@@ -37,36 +37,19 @@ namespace ED2.ATV1
                         Console.WriteLine("Sair");
                         break;
                     case 1:
-                        Console.WriteLine("Cadastrar");
+                        Cadastrar(equipe);
                         break;
                     case 2:
-                        int Id;
-                        Console.WriteLine("Consultar");
-                        Console.WriteLine("Digite o ID do vendedor: ");
-                        
-                        Id = int.Parse(Console.ReadLine());
-                        Vendedor vendedor = new Vendedor(Id, "", 0);
-                        
-                        vendedor = equipe.searchVendedor(vendedor);
-                        
-                        if(vendedor.Id == 0)
-                        {
-                            Console.WriteLine("Vendedor não encontrado");
-                        }
-                        else
-                        {
-                            Console.WriteLine(vendedor.ToString());
-                        }
-
+                        Consultar(equipe);
                         break;
                     case 3:
-                        Console.WriteLine("Excluir vendedor");
+                        Remover(equipe);
                         break;
                     case 4:
-                        Console.WriteLine("Registrar venda");
+                        Registrar(equipe);
                         break;
                     case 5:
-                        Console.WriteLine("Listar vendedores");
+                        Listar(equipe);
                         break;
                     default:
                         Console.WriteLine("opção invalida");
@@ -77,6 +60,145 @@ namespace ED2.ATV1
             }
 
             //
+        }
+
+        private static void Cadastrar(Vendedores vendedores)
+        {
+            Console.WriteLine("Cadastrar");
+
+            //Console.WriteLine("Digite o nome do vendedor: ");
+            //string nome = Console.ReadLine();
+
+            //Console.WriteLine("Digite o ID do vendedor: ");
+            //int Id = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Digite o ID do vendedor: ");
+            //double percComissao = double.Parse(Console.ReadLine());
+
+            //Vendedor vendedor = new Vendedor(Id, nome, percComissao);
+
+
+            Vendedor vendedor = new Vendedor();
+
+
+            Console.WriteLine("Digite o nome do vendedor: ");
+            vendedor.Nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o ID do vendedor: ");
+            vendedor.Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o percentual do vendedor: ");
+            vendedor.PercComissao = double.Parse(Console.ReadLine());
+
+            bool cadastradoComSucesso = vendedores.addVendedor(vendedor);
+
+            if (cadastradoComSucesso)
+            {
+                Console.WriteLine("Cadastrado com sucesso");
+            }
+
+            else
+            {
+                Console.WriteLine("Limite de vendedores atingido");
+            }
+
+
+        }
+        
+        private static void Consultar(Vendedores vendedores)
+        {
+                        
+            Vendedor vendedor = new Vendedor();
+
+            Console.WriteLine("Consultar");
+
+            Console.WriteLine("Digite o ID do vendedor: ");
+
+            vendedor.Id = int.Parse(Console.ReadLine());
+
+            vendedor = vendedores.searchVendedor(vendedor);
+
+            if (vendedor.Id == 0)
+            {
+                Console.WriteLine("Vendedor não encontrado");
+            }
+            else
+            {
+                Console.WriteLine(vendedor.ToString());
+            }
+
+        }
+
+        private static void Listar(Vendedores vendedores)
+        {
+            foreach(Vendedor vendedorCadavez in vendedores.OsVendedores)
+            {
+                if (vendedorCadavez.Id != 0)
+                {
+                    Console.WriteLine(vendedorCadavez.ToString());
+                }
+            }
+        }
+
+        private static void Registrar(Vendedores vendedores)
+        {
+            Vendedor vendedor = new Vendedor();
+            Venda vendaEspecifica = new Venda();
+
+            Console.WriteLine("Digite o ID do vendedor: ");
+            vendedor.Id = int.Parse(Console.ReadLine());
+
+            vendedor = vendedores.searchVendedor(vendedor);
+
+            if (vendedor.Id == 0)
+            {
+                Console.WriteLine("Vendedor não encontrado");
+
+                return;
+            }
+            
+            Console.WriteLine("Digite o valor: ");
+            vendaEspecifica.Valor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite quantos produtos vendeu: ");
+            vendaEspecifica.Qtde = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o dia que vendeu: ");
+            int dia = int.Parse(Console.ReadLine());
+
+            vendedor.registrarVenda(dia, vendaEspecifica);
+
+        }
+
+        private static void Remover(Vendedores vendedores)
+        {
+            Vendedor vendedorRemovido = new Vendedor();
+            
+            Console.WriteLine("Digite o ID do vendedor que irá remover: ");
+            vendedorRemovido.Id = int.Parse(Console.ReadLine());
+
+            vendedorRemovido = vendedores.searchVendedor(vendedorRemovido);
+            
+            if (vendedorRemovido.Id == 0)
+            {
+                Console.WriteLine("Vendedor não encontrado");
+
+                return;
+            }
+
+            if (vendedorRemovido.valorVendas() == 0)
+            {
+                vendedores.delVendedor(vendedorRemovido);
+                Console.WriteLine("Vendedor removido com sucesso");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Vendedor não pôde ser removido, pois há vendas associadas a ele");
+            }
+
+
         }
     }
 }
